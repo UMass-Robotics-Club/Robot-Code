c6536ffd70819c8e2ab3de2b69b0f907b318a8d4
Kedar Prasad <kdr.prsd@gmail.com>
2025-04-07 00:02:48 -0400

    add fast stream

---
+++ actcode/save.py
@@ -0,0 +1,23 @@
+import grpc
+from concurrent import futures
+import robot_call_pb2
+import robot_call_pb2_grpc
+import struct
+
+class RobotCallServicer(robot_call_pb2_grpc.RobotCallServicer):
+    def SendInstructions(self, request, context):
+        print(f"Received command with FOLLOWING: {request}")    
+        print([struct.pack('f', i) for i in request[0:6]])
+        return robot_call_pb2.Response(transmitted=True, message="Instructions received")
+
+def serve():
+    server = grpc.server(futures.ThreadPoolExecutor(max_workers=10))
+    robot_call_pb2_grpc.add_RobotCallServicer_to_server(RobotCallServicer(), server)
+    server.add_insecure_port('[::]:50051')
+    server.start()
+    print("Server is running on port 50051...")
+    server.wait_for_termination()
+
+if __name__ == "__main__":
+    serve()
+
